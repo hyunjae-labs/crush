60915ef5ce86a3d0238949af750ef65c635ba8d0
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-25 10:03:30 -0300

    test: more cases
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/home/home_test.go
+++ internal/home/home_test.go
@@ -14,9 +14,13 @@ func TestDir(t *testing.T) {
 func TestShort(t *testing.T) {
 	d := filepath.Join(Dir(), "documents", "file.txt")
 	require.Equal(t, "~/documents/file.txt", Short(d), "should replace home directory with ~")
+	ad := "/absolute/path/file.txt"
+	require.Equal(t, ad, Short(ad))
 }
 
 func TestLong(t *testing.T) {
 	d := "~/documents/file.txt"
 	require.Equal(t, filepath.Join(Dir(), "documents", "file.txt"), Long(d), "should replace ~ with home directory")
+	ad := "/absolute/path/file.txt"
+	require.Equal(t, ad, Long(ad))
 }
